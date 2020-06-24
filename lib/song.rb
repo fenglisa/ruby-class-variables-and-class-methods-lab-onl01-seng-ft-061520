c46d055b6a1a0c4ceeee1ed343e065aa309ef9e3
Lisa Feng <lisafeng10@gmail.com>
2020-06-24 18:52:14 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -29,7 +29,7 @@ class Song
   def self.genre_count
     genreCount = {}
     @@genres.each do |genre| 
-      genreCount[genre] = individualCount += 1
+      genreCount[genre] = genreCount[genre].length
     end
     genreCount
   end
