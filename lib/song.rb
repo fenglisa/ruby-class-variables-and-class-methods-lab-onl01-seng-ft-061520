ad4f4aad39c8f267a362d681aabbe8c8ae78e1a4
Lisa Feng <lisafeng10@gmail.com>
2020-06-24 18:16:48 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,8 +1,7 @@
 class Song
   attr_accessor :name, :artist, :genre
-  @@
+  @@song_count = 0
   def initialize(name, artist, genre)
-    @name = name
-    @artist = artist
-    @genre = genre
+    @@song_count += 1
+  end
 end
